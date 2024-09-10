aa247abbf491287ff94eccebeaef84a8acb6252a
randhale <randhale@fortinet.com>
2024-09-10 12:33:26 +0530

    Addressed review changes

--- ibm-randori/operations.py
+++ ibm-randori/operations.py
@@ -25,8 +25,8 @@ class IBMRandori:
 
     def make_request(self, endpoint, params=None, data=None, method='GET'):
         try:
-            headers = {'Authorization': self.api_token, 'Content-Type': 'application/json'}
-            url = '{0}{1}{2}'.format(self.base_url, '/recon/api/v1', endpoint)
+            headers = {'Authorization': "Bearer " + self.api_token, 'Content-Type': 'application/json'}
+            url = '{0}{1}'.format(self.base_url, endpoint)
             logger.info('Request URL {0}'.format(url))
 
             # CURL UTILS CODE
@@ -81,7 +81,27 @@ def get_ips(config, params):
     params = obj.build_payload(params)
     if params.get('q'):
         params['q'] = obj.encode_query(params.get('q'))
-    response = obj.make_request(endpoint='/ip', params=params)
+    response = obj.make_request(endpoint='/recon/api/v1/ip', params=params)
+    return response
+
+
+def get_ips_for_hostname(config, params):
+    obj = IBMRandori(config)
+    # params.update({"sort": "-target_temptation"})
+    params = obj.build_payload(params)
+    if params.get('q'):
+        params['q'] = obj.encode_query(params.get('q'))
+    response = obj.make_request(endpoint='/recon/api/v1/ips-for-hostname', params=params)
+    return response
+
+
+def get_ips_for_network(config, params):
+    obj = IBMRandori(config)
+    # params.update({"sort": "-target_temptation"})
+    params = obj.build_payload(params)
+    if params.get('q'):
+        params['q'] = obj.encode_query(params.get('q'))
+    response = obj.make_request(endpoint='/recon/api/v1/ips-for-network', params=params)
     return response
 
 
@@ -90,7 +110,7 @@ def get_hostnames(config, params):
     params = obj.build_payload(params)
     if params.get('q'):
         params['q'] = obj.encode_query(params.get('q'))
-    response = obj.make_request(endpoint='/hostname', params=params)
+    response = obj.make_request(endpoint='/recon/api/v1/hostname', params=params)
     return response
 
 
@@ -99,7 +119,7 @@ def get_networks(config, params):
     params = obj.build_payload(params)
     if params.get('q'):
         params['q'] = obj.encode_query(params.get('q'))
-    response = obj.make_request(endpoint='/network', params=params)
+    response = obj.make_request(endpoint='/recon/api/v1/network', params=params)
     return response
 
 
@@ -108,7 +128,7 @@ def get_targets(config, params):
     params = obj.build_payload(params)
     if params.get('q'):
         params['q'] = obj.encode_query(params.get('q'))
-    response = obj.make_request(endpoint='/target', params=params)
+    response = obj.make_request(endpoint='/recon/api/v1/target', params=params)
     return response
 
 
@@ -117,7 +137,59 @@ def get_services(config, params):
     params = obj.build_payload(params)
     if params.get('q'):
         params['q'] = obj.encode_query(params.get('q'))
-    response = obj.make_request(endpoint='/service', params=params)
+    response = obj.make_request(endpoint='/recon/api/v1/service', params=params)
+    return response
+
+
+def get_all_detections_for_target(config, params):
+    obj = IBMRandori(config)
+    params = obj.build_payload(params)
+    if params.get('q'):
+        params['q'] = obj.encode_query(params.get('q'))
+    response = obj.make_request(endpoint='/recon/api/v1/all-detections-for-target', params=params)
+    return response
+
+
+def get_single_detection_for_target(config, params):
+    obj = IBMRandori(config)
+    params = obj.build_payload(params)
+    if params.get('q'):
+        params['q'] = obj.encode_query(params.get('q'))
+    response = obj.make_request(endpoint='/recon/api/v1/single-detection-for-target', params=params)
+    return response
+
+
+def get_policy(config, params):
+    obj = IBMRandori(config)
+    params = obj.build_payload(params)
+    if params.get('q'):
+        params['q'] = obj.encode_query(params.get('q'))
+    response = obj.make_request(endpoint='/recon/api/v1/policy', params=params)
+    return response
+
+
+def get_report(config, params):
+    obj = IBMRandori(config)
+    params = obj.build_payload(params)
+    if params.get('q'):
+        params['q'] = obj.encode_query(params.get('q'))
+    response = obj.make_request(endpoint='/recon/api/v1/report', params=params)
+    return response
+
+
+def get_statistics(config, params):
+    obj = IBMRandori(config)
+    params = obj.build_payload(params)
+    if params.get('q'):
+        params['q'] = obj.encode_query(params.get('q'))
+    response = obj.make_request(endpoint='/recon/api/v1/statistics', params=params)
+    return response
+
+
+def get_artifact(config, params):
+    obj = IBMRandori(config)
+    response = obj.make_request(endpoint='/artifactstore/api/v1/artifact/{artifact_uuid}'.
+                                format(artifact_uuid=params.get('artifact_uuid')),params=params)
     return response
 
 
@@ -134,8 +206,16 @@ def _check_health(config):
 
 operations = {
     "get_ips": get_ips,
+    "get_ips_for_hostname": get_ips_for_hostname,
+    "get_ips_for_network": get_ips_for_network,
     "get_hostnames": get_hostnames,
     "get_networks": get_networks,
     "get_targets": get_targets,
-    "get_services": get_services
+    "get_services": get_services,
+    "get_all_detections_for_target": get_all_detections_for_target,
+    "get_single_detection_for_target": get_single_detection_for_target,
+    "get_policy": get_policy,
+    "get_report": get_report,
+    "get_statistics": get_statistics,
+    "get_artifact": get_artifact
 }
